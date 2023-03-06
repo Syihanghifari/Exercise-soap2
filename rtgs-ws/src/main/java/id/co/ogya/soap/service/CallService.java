5a5c358db5ac1c862cfeafcbd7ca61f42bec9d15
Syihanghifari <syihanghifari27@gmail.com>
2023-03-06 14:50:28 +0700

    adjustment

--- rtgs-ws/src/main/java/id/co/ogya/soap/service/CallService.java
+++ rtgs-ws/src/main/java/id/co/ogya/soap/service/CallService.java
@@ -15,21 +15,28 @@ public class CallService implements TransferService {
 		id.co.ogya.ebanking.ejb.request.TransferRequest data = new id.co.ogya.ebanking.ejb.request.TransferRequest();
 		id.co.ogya.ebanking.ejb.response.TransferResponse dataResponse = new id.co.ogya.ebanking.ejb.response.TransferResponse();
 		String simpleDataSourceAccessJNDIName = "EbankingServiceImpl#id.co.ogya.ebanking.ejb.EbankingService";
-		try {
-			ServiceFactory serviceFactory = new ServiceFactory();
-			EbankingService ebankingService = (EbankingService) serviceFactory
-					.getService(simpleDataSourceAccessJNDIName);
-			data.setAccountFrom(100L);
-			data.setAccountTo(transferRequest.getAccountNo());
-			data.setAmount(transferRequest.getAmount());
-			data.setNotes(transferRequest.getNotes());
-			dataResponse = ebankingService.kirim(data);
-			response.setErrorCode(dataResponse.getErrorCode());
-			response.setErrorMessage(dataResponse.getErrorMessage());
-			response.setReferenceNumber(dataResponse.getReferenceNumber());
-		}catch(Exception e) {
-			e.printStackTrace();
+		if(transferRequest.getBankCode() != 1L) {
+			try {
+				ServiceFactory serviceFactory = new ServiceFactory();
+				EbankingService ebankingService = (EbankingService) serviceFactory
+						.getService(simpleDataSourceAccessJNDIName);
+				data.setAccountFrom(100L);
+				data.setAccountTo(transferRequest.getAccountNo());
+				data.setAmount(transferRequest.getAmount());
+				data.setNotes(transferRequest.getNotes());
+				data.setBranchCode(transferRequest.getBankCode());
+				dataResponse = ebankingService.transferRtgs(data);
+				response.setErrorCode(dataResponse.getErrorCode());
+				response.setErrorMessage(dataResponse.getErrorMessage());
+				response.setReferenceNumber(dataResponse.getReferenceNumber());
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}else {
+			response.setErrorCode("OGYA-999");
+			response.setErrorMessage("nomor bank tidak terdaftar");
 		}
+		
 		return response;
 	}
 }
